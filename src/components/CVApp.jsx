3e0b0f3855e84a6904720f0056db732dcd52f00f
birando <birger1@live.com>
2024-11-19 23:31:33 +0100

    feat: add general styling and first component draft

--- src/components/CVApp.jsx
+++ src/components/CVApp.jsx
@@ -4,12 +4,43 @@ import Education from "./Education";
 import Experience from "./Experience";
 
 function CVApp() {
+  const [filled, setFilled] = useState({
+    name: false,
+    email: false,
+    phone: false,
+  });
+
+  const alertMessage = "Write something before adding";
+
+  const handleNameClick = (nameInput) => {
+    if (nameInput.trim() == "") return alert(alertMessage);
+    setFilled({ ...filled, name: !filled.name });
+  };
+
+  const handleEmailClick = (emailInput) => {
+    if (emailInput.trim() == "") return alert(alertMessage);
+    setFilled({ ...filled, email: !filled.email });
+  };
+
+  const handlePhoneClick = (phoneInput) => {
+    if (phoneInput.trim() == "") return alert(alertMessage);
+    setFilled({ ...filled, phone: !filled.phone });
+  };
+
   return (
-    <>
-      <GeneralInfo />
+    <div>
+      <h1>CV form</h1>
+      <GeneralInfo
+        nameFilled={filled.name}
+        emailFilled={filled.email}
+        phoneFilled={filled.phone}
+        handleNameClick={handleNameClick}
+        handleEmailClick={handleEmailClick}
+        handlePhoneClick={handlePhoneClick}
+      />
       <Education />
       <Experience />
-    </>
+    </div>
   );
 }
 
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,18 +1,88 @@
 import { useState } from "react";
 
-export default function GeneralInfo() {
+export default function GeneralInfo({
+  nameFilled,
+  emailFilled,
+  phoneFilled,
+  handleNameClick,
+  handleEmailClick,
+  handlePhoneClick,
+}) {
+  const [nameInput, setNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [phoneInput, setPhoneInput] = useState("");
+
+  const handleNameChange = (e) => {
+    setNameInput(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmailInput(e.target.value);
+  };
+
+  const handlePhoneChange = (e) => {
+    setPhoneInput(e.target.value);
+  };
+
   return (
     <section>
       <h2>General Information</h2>
+      <button onClick={() => handleNameClick(nameInput)}>
+        {nameFilled ? "Edit" : "Add"}
+      </button>
       <label htmlFor="fullName">Full name:</label>
+      <span style={nameFilled ? { display: "inline" } : { display: "none" }}>
+        {" "}
+        {nameInput}
+      </span>
+      <br style={nameFilled ? { display: "none" } : { display: "inline" }} />
+      <input
+        style={nameFilled ? { display: "none" } : { display: "inline" }}
+        value={nameInput}
+        onChange={handleNameChange}
+        id="fullName"
+        type="text"
+        placeholder="Tom Johnson"
+      />
       <br />
-      <input id="fullName" type="text" /> <br />
+
+      <button onClick={() => handleEmailClick(emailInput)}>
+        {emailFilled ? "Edit" : "Add"}
+      </button>
       <label htmlFor="email">Email: </label>
+      <span style={emailFilled ? { display: "inline" } : { display: "none" }}>
+        {" "}
+        {emailInput}
+      </span>
+      <br style={emailFilled ? { display: "none" } : { display: "inline" }} />
+      <input
+        style={emailFilled ? { display: "none" } : { display: "inline" }}
+        value={emailInput}
+        onChange={handleEmailChange}
+        id="email"
+        type="email"
+        placeholder="tom@email.com"
+      />
       <br />
-      <input id="email" type="text" /> <br />
+
+      <button onClick={() => handlePhoneClick(phoneInput)}>
+        {phoneFilled ? "Edit" : "Add"}
+      </button>
       <label htmlFor="phoneNumber">Phone number: </label>
+      <span style={phoneFilled ? { display: "inline" } : { display: "none" }}>
+        {" "}
+        {phoneInput}
+      </span>
+      <br style={phoneFilled ? { display: "none" } : { display: "inline" }} />
+      <input
+        style={phoneFilled ? { display: "none" } : { display: "inline" }}
+        value={phoneInput}
+        onChange={handlePhoneChange}
+        id="phoneNumber"
+        type="tel"
+        placeholder="123 45 678"
+      />
       <br />
-      <input id="phoneNumber" type="text" />
     </section>
   );
 }
